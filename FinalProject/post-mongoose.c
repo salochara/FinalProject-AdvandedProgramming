721937da37a88ec6a7cccfb71cd7f99569795a2e
Salo Charabati <salochara@gmail.com>
2019-12-06 13:59:19 -0600

    compute pi woking

--- FinalProject/post-mongoose.c
+++ FinalProject/post-mongoose.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "mongoose.h"
 
+#define BUFFER_SIZE 50
 
 
 
@@ -20,6 +21,26 @@ struct mg_str cb(struct mg_connection *c, struct mg_str file_name) {
     // fname is user-controlled and needs to be sanitized.
     printf("%s file name\n",file_name);
     return file_name;
+}
+
+double computePI(char * buffer)
+{
+    // Approximation to pi using the following formula :
+    // pi = 4 - (4/3) + (4/5) - (4/7) + (4/9) - ...
+    double result = 4;
+    int sign = -1;
+    unsigned long int divisor = 3;
+
+    // Casting the string received into an int
+    int iterations = atoi(buffer);
+
+    for (int i = 0; i < iterations; ++i)
+    {
+        result += sign * (4.0 / divisor);
+        sign *= -1;
+        divisor += 2;
+    }
+    return result;
 }
 
 
@@ -47,14 +68,23 @@ void event_handler(struct mg_connection * nc, int event, void * p)
         case MG_EV_HTTP_REQUEST:
             if (mg_vcmp(&hm->uri, "/save") == 0)
             {
-                char result[100];
-                mg_get_http_var(&hm->body, "salo", result,
-                                100);
-                //printf("in /save\n");
-                //sleep(1);
-                printf("setting %s\n",result);
-                mg_send_http_chunk(nc,result, sizeof(result));
-                //mg_http_send_redirect(nc, 302, mg_mk_str("/"), mg_mk_str(NULL));
+                char buffer[BUFFER_SIZE];
+
+                // Get the amount of iterations to be done from the html form
+                mg_get_http_var(&hm->body, "iterations", buffer,sizeof(buffer));
+
+                // Use chunked encoding in order to avoid calculating Content-Length
+                mg_printf(nc, "%s", "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
+
+                // Compute PI with the amount of iterations received from the user
+                double result = computePI(buffer);
+
+                // Output JSON object which holds the result of PI
+                mg_printf_http_chunk(nc, "{ \"result\": %f }", result);
+
+                // Send empty chunk, the end of response
+                mg_send_http_chunk(nc, "", 0);
+
 
             }
             // For index to show
